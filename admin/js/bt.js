fa687f4e06e2e682987f7889d073a59ff985b1e1
Rahman <rahmanromli@gmail.com>
2019-03-08 02:43:44 +0800

    Add show title

--- admin/js/bt.js
+++ admin/js/bt.js
@@ -10,20 +10,33 @@ window.addEvent("domready",function(){
     if(jQuery(this).parents(parent).css('display')=='none' ) return;
 		jQuery(this).change(function(){
       var name = this.id.replace('jform_params_','');
-      console.log(name)
 			jQuery(this).find('option').each(function(){
-        console.log('.'+name+'_'+this.value)
-        jQuery('.'+name+'_'+this.value).each(function(){
+				jQuery('.'+name+'_'+this.value).each(function(){
           jQuery(this).parents(parent).hide();
         })
       })
-				
 			jQuery('.'+name+'_'+jQuery(this).find('option:selected').val()).each(function(){
 				jQuery(this).parents(parent).fadeIn(500);
 			})
 		});
 		jQuery(this).change();
-	});
+	});	// FlipClock Type
+	
+	jQuery('.toggle').each(function() {
+		jQuery(this).change(function() {
+			var name = this.id.replace('jform_params_','')
+			var inp = jQuery('*[class^="'+ name +'_"]')
+			jQuery(inp).each(function() {	// Hide first
+				jQuery(this).parent().parent().hide()
+			})
+			console.log(this)
+			console.log(this.children)
+			if (jQuery(this).find('input').prop('checked')) {
+				jQuery(inp).each(function() {
+					jQuery(this).parent().parent().fadeIn(400)
+				})
+			}
+		})
+		jQuery(this).change()
+	})
 })
-
-
